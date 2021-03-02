465942e474fe0c5966f1dbcbc09d068924884155
Kush Patel <kushpatel.inexture@gmail.com>
2021-03-02 21:40:14 +0530

    removed static file line

--- doctor_appointment_system/settings.py
+++ doctor_appointment_system/settings.py
@@ -134,4 +134,4 @@ MEDIA_URL = "/media/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
