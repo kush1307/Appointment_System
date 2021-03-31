cfaa8f9f14e9c8e62ac62b8c512471c993c501a2
Kush Patel <kushpatel.inexture@gmail.com>
2021-03-31 20:11:38 +0530

    doctor appointment, patient take appointment.

--- appointment/urls.py
+++ appointment/urls.py
@@ -4,7 +4,7 @@ doctor_appointment_system URL Configuration
 """
 
 from django.urls import path
-from appointment.views import HomePageView
+from appointment.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -13,15 +13,15 @@ app_name = 'appointment'
 
 urlpatterns = [
 
-    path('', HomePageView, name='home'),
-    # path('service', ServiceView.as_view(), name='service'),
-    # path('doctor/appointment/create', AppointmentCreateView.as_view(), name='doctor-appointment-create'),
-    # path('doctor/appointment/', AppointmentListView.as_view(), name='doctor-appointment'),
-    # path('<pk>/delete/', AppointmentDeleteView.as_view(), name='delete-appointment'),
-    # path('<pk>/patient/delete', PatientDeleteView.as_view(), name='delete-patient'),
-    # path('patient-take-appointment/<pk>', TakeAppointmentView.as_view(), name='take-appointment'),
-    # path('search/', SearchView.as_view(), name='search'),
-    # path('patient/', PatientListView.as_view(), name='patient-list'),
+    path('', HomePageView.as_view(), name='home'),
+    path('service', ServiceView.as_view(), name='service'),
+    path('doctor/appointment/create', AppointmentCreateView.as_view(), name='doctor-appointment-create'),
+    path('doctor/appointment/', AppointmentListView.as_view(), name='doctor-appointment'),
+    path('<pk>/delete/', AppointmentDeleteView.as_view(), name='delete-appointment'),
+    path('<pk>/patient/delete', PatientDeleteView.as_view(), name='delete-patient'),
+    path('patient-take-appointment/<pk>', TakeAppointmentView.as_view(), name='take-appointment'),
+    path('search/', SearchView.as_view(), name='search'),
+    path('patient/', PatientListView.as_view(), name='patient-list'),
     #path('patients/<int:appointment_id>', PatientPerAppointmentView.as_view(), name='patient-list'),
 
 
